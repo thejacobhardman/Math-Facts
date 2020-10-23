571ad113b7f7be6bcd22f9590c9d2ffe6ff7d35d
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2020-10-23 15:22:41 -0600

    simplify mathfact data class

--- app/src/main/java/com/example/mathfacts/MathFact.kt
+++ app/src/main/java/com/example/mathfacts/MathFact.kt
@@ -1,22 +1,8 @@
 package com.example.mathfacts
 
-import com.google.gson.annotations.Expose
-import com.google.gson.annotations.SerializedName
-
-class MathFact {
-    @SerializedName("text")
-    @Expose
-    var text: String? = null
-
-    @SerializedName("number")
-    @Expose
-    var number: Int? = null
-
-    @SerializedName("found")
-    @Expose
-    var found: Boolean? = null
-
-    @SerializedName("type")
-    @Expose
-    var type: String? = null
-}
+data class MathFact (
+    val text: String,
+    val number: String,
+    val found: Boolean,
+    val type: String
+) { }
