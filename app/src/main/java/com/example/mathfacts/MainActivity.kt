b86fbb26b596b73aaeb8135661815697f71c2d58
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2020-10-23 14:49:47 -0600

    add network check method and network permissions

--- app/src/main/java/com/example/mathfacts/MainActivity.kt
+++ app/src/main/java/com/example/mathfacts/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.example.mathfacts
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +10,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun isNetworkConnected(): Boolean {
+        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork = connectivityManager.activeNetwork
+        val networkCapabilities = connectivityManager.getNetworkCapabilities(activeNetwork)
+        return networkCapabilities != null && networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
     }
 }
