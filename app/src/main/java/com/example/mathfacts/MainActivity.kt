5016e69b16edf50d89179e1c95021423e290ff33
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2020-10-23 16:06:04 -0600

    implement get request

--- app/src/main/java/com/example/mathfacts/MainActivity.kt
+++ app/src/main/java/com/example/mathfacts/MainActivity.kt
@@ -6,13 +6,17 @@ import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val mathFactRetriever = MathFactRetriever()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         if (isNetworkConnected()) {
-            //getMathFact()
+            getMathFact()
         } else {
             AlertDialog.Builder(this).setTitle("No Internet Connection!")
                 .setMessage("Please check your internet connection and try again.")
@@ -28,5 +32,25 @@ class MainActivity : AppCompatActivity() {
         val activeNetwork = connectivityManager.activeNetwork
         val networkCapabilities = connectivityManager.getNetworkCapabilities(activeNetwork)
         return networkCapabilities != null && networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+    }
+
+    fun getMathFact() {
+        val mainActivityJob = Job()
+
+        val errorHandler =
+            CoroutineExceptionHandler { _, exception ->
+                AlertDialog.Builder(this).setTitle("Error!")
+                    .setMessage(exception.message)
+                    .setPositiveButton(android.R.string.ok) { _, _ -> }
+                    .setIcon(android.R.drawable.ic_dialog_alert).show()
+            }
+
+        val coroutineScope =
+            CoroutineScope(mainActivityJob + Dispatchers.Main)
+        coroutineScope.launch(errorHandler) {
+
+            val result = mathFactRetriever.getMathFact()
+            //txtMathFact.text = result.text
+        }
     }
 }
