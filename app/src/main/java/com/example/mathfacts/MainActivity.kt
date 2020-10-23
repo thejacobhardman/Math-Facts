23d023e877b61da494ea3b3e5ed553b626fba9be
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2020-10-23 15:02:54 -0600

    implement network check method and add error message

--- app/src/main/java/com/example/mathfacts/MainActivity.kt
+++ app/src/main/java/com/example/mathfacts/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.mathfacts
 
+import android.app.AlertDialog
 import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
@@ -9,6 +10,16 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if (isNetworkConnected()) {
+            //getMathFact()
+        } else {
+            AlertDialog.Builder(this).setTitle("No Internet Connection!")
+                .setMessage("Please check your internet connection and try again.")
+                .setPositiveButton(android.R.string.ok) {_, _ ->}
+                .setIcon(android.R.drawable.ic_dialog_alert).show()
+        }
+
         setContentView(R.layout.activity_main)
     }
 
